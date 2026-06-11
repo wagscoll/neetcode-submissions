8de1d6dcdfe926e1b80798aff454196253523594
Collin Wagstaff <collin.wagstaff@gmail.com>
2026-06-11 12:05:08 -0600

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] answer = new int[nums.Length];
+
+        for(int itterator = 0; itterator < nums.Length; itterator++)
+        {
+            int result = 1;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(itterator == i) 
+                    continue;
+
+                result *= nums[i];
+            }
+            answer[itterator] = result;
+        }
+        return answer;
+    }
+}
