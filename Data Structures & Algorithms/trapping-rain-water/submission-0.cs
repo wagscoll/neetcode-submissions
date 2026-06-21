2b953e57067e758b1231c0b1ecbe86dbde9d3eab
Collin Wagstaff <collin.wagstaff@gmail.com>
2026-06-20 22:23:53 -0600

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int Trap(int[] height) 
+    {
+        int area = 0;
+        int left = 0;
+        int right = height.Length-1;
+
+        int distance = 0;
+        int leftMax = -1;
+        int rightMax = -1;
+
+        while(left < right)
+        {
+
+            if(height[left] > leftMax)
+                leftMax = height[left];
+            
+            if(height[right] > rightMax)
+                rightMax = height[right]; 
+
+            if(height[left] < height[right])
+            {
+                area += (leftMax - height[left]);
+                left++;
+            }
+            else
+            {
+                area += (rightMax - height[right]);
+                right--;
+            }
+        }
+
+        return area;
+    }
+}
