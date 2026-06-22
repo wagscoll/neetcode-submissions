4af93e93c718b35bcec17116c2653cec49c888b2
Collin Wagstaff <collin.wagstaff@gmail.com>
2026-06-21 20:14:08 -0600

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures){
+        int[] answer = new int[temperatures.Length];
+        
+        for(int i = 0; i < temperatures.Length; i++){
+            int next = 0;
+            
+            for(int j = i+1; j < temperatures.Length; j++){
+                if(temperatures[j] > temperatures[i]){
+                    next = (j-i);
+                    break;
+                }
+            }
+            answer[i] = next;
+        }
+        return answer;
+    }
+}
