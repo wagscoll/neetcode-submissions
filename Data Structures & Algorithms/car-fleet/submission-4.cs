e59505a10245268ef05c7ca990719cd12b97a6f9
Collin Wagstaff <collin.wagstaff@gmail.com>
2026-06-28 22:08:08 -0600

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int CarFleet(int target, int[] position, int[] speed) 
+    {
+        var cars = new(int pos, int speed)[position.Length];
+        
+        int answer = 0;
+        double leaderTTT = 0;
+
+        for(int i = 0; i < position.Length; i++)           
+            cars[i] = (position[i], speed[i]);
+        
+        var carsByPosition = cars.OrderByDescending(c => c.pos);
+        foreach (var c in carsByPosition)
+        {
+            double timeToTarget = (double)(target - c.pos) / c.speed;
+            if (timeToTarget > leaderTTT) 
+            {
+                leaderTTT = timeToTarget;
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
