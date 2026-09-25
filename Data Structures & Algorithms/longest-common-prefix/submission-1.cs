96156eec469d50e8a585c4d576579430718937fa
Collin Wagstaff <collin.wagstaff@gmail.com>
2026-09-24 21:06:43 -0600

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) 
+    {
+
+        if(strs.Length == 0)
+            return "";
+        
+        string temp_str = strs[0];
+        
+        for(int i = 1; i < strs.Length; i++)
+        {
+            if(strs[i].Contains(temp_str))
+            {
+                Console.WriteLine("Continue has been selected.");
+                continue;
+            }
+            while(!strs[i].Contains(temp_str))
+            {
+                temp_str = temp_str.Substring(0, temp_str.Length-1);
+                Console.WriteLine($"While Loop has been entered . . . \n\n temp_str == {temp_str}");
+
+                i--;
+            }
+        }
+     
+        return temp_str;
+    }
+}
